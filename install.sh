27a4eb4eaafa0927f0a804934e0b963778dd874e
Matt Fair <matt.fair@gmail.com>
2017-11-13 02:18:26 -0500

    Added .tmux.conf

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ echo "Installing VIM configuration"
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-cp -r .vimrc ~/
+cp .vimrc ~/
 vim +PluginInstall +qall
+
+echo "Installing TMUX configuration"
+cp .tmux.conf ~/ 
