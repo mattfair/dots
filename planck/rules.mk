77f15b571dc71c99a851d29b0dfecf0fff1204e9
Matt Fair <matt.fair@gmail.com>
2017-12-10 13:03:36 -0500

    get changes to flash planck

--- planck/rules.mk
+++ planck/rules.mk
@@ -1,5 +1,6 @@
-BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 COMMAND_ENABLE   = no   # Disable shift combination, which conflicts with shift-parens
 MOUSEKEY_ENABLE  = yes  # Mouse keys 
 NKRO_ENABLE      = yes  # N-key rollover required for use as a steno board
-AUDIO_ENABLE     = yes  # Audio output on port C6
+AUDIO_ENABLE     = no  # Audio output on port C6
+MIDI_ENABLE      = no   # required for MOUSEKEY_ENABLE to work
